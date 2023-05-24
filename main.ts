25151ba1d7c6e8414925be181e85748edb25becd
hectorelmas <124789223+hmorhid1407@users.noreply.github.com>
2023-05-24 13:10:41 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,17 @@
+basic.forever(function () {
+    for (let index = 0; index <= 4; index++) {
+        led.plot(0, 2)
+    }
+    for (let index = 0; index <= 4; index++) {
+        led.plot(1, 2)
+    }
+    for (let index = 0; index <= 4; index++) {
+        led.plot(2, 2)
+    }
+    for (let index = 0; index <= 4; index++) {
+        led.plot(3, 2)
+    }
+    for (let index = 0; index <= 4; index++) {
+        led.plot(4, 2)
+    }
+})
